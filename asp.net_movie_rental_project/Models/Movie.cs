189307d27d3ee818bbb7c2732a76d1250998f277
William Stephens <william.a.stephens17@gmail.com>
2020-08-24 17:34:21 -0500

    Digesting MVC

--- asp.net_movie_rental_project/Models/Movie.cs
+++ asp.net_movie_rental_project/Models/Movie.cs
@@ -3,8 +3,13 @@ namespace asp.net_movie_rental_project.Models
 {
     public class Movie
     {
+        public int id { get; set; }
+        public string name { get; set; }
+
         public Movie()
         {
         }
+
+        // /movies/random
     }
 }
