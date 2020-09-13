ae3978eb412d577b94f542bb7732114b7ea297b4
William Stephens <william.a.stephens17@gmail.com>
2020-09-12 21:09:22 -0500

    .

---
+++ asp.net_movie_rental_project/Controllers/StoreController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asp.net_movie_rental_project.Controllers
+{
+    public class StoreController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+    }
+}
